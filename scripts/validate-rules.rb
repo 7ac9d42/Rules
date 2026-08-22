19e81077db6f77973bcd395850d9c83512614b12
7ac9d42 <110989625+7ac9d42@users.noreply.github.com>
2026-08-22 18:11:17 +0800

    fix: 为高风控类别补充独立选择组

--- scripts/validate-rules.rb
+++ scripts/validate-rules.rb
@@ -511,7 +511,7 @@ begin
 
   airport1_service_groups = %w[
     GoogleVPN Google Meta Microsoft Discord Talkatone LINE Signal TikTok NETFLIX DisneyPlus HBO
-    Primevideo AppleTV Spotify Global-TV Global-Media Wise 国外电商
+    Primevideo AppleTV Spotify Global-TV Global-Media 通信服务 金融服务 加密资产 Wise 国外电商
   ]
   airport1_service_groups.each do |name|
     group = groups.find { |candidate| candidate["name"] == name }
@@ -595,9 +595,9 @@ begin
   end
 
   quality_category_routes = {
-    "finance_domain" => "机场名称1优先",
-    "cryptocurrency_domain" => "机场名称1优先",
-    "communication_domain" => "机场名称1优先",
+    "finance_domain" => "金融服务",
+    "cryptocurrency_domain" => "加密资产",
+    "communication_domain" => "通信服务",
     "ecommerce_domain" => "国外电商",
   }
   quality_category_routes.each do |provider, target|
